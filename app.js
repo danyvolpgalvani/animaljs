89dbe08c73371f5e9fe07d50889738fc7b3ab024
Daniele Volpato <volpato.daniele@iisgalvanimi.edu.it>
2020-01-08 18:25:03 +0000

    Preparazione alla ri prova

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+ res.render('index');
+});
+
+app.get('/api/album-animali', (req, res) => {
+  res.sendfile(__dirname + '/public/json/demo-animals.json')
+});
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
